343b1f57cae295e3847000ec45f1b6bda3ee9443
Dennis R <dennis.riffo2015@alu.uct.cl>
2018-11-27 08:46:04 -0300

    Comentarios
    comentario explicativo en nuevos archivos escribir y leer del chat

--- Proyecto/public/escribir.php
+++ Proyecto/public/escribir.php
@@ -1,6 +1,10 @@
 <?php
-	$mensaje = "<b>".$_POST['usuario']."</b>"."dice: ".$_POST['mensaje']."<br>"; //los datos recibidos por el js los 
-	$file = fopen("chat.txt", "a");
-	fwrite($file, $mensaje . PHP_EOL);
-	fclose($file);
+	$mensaje = "<b>".$_POST['usuario']."</b>"."dice: ".$_POST['mensaje']."<br>"; //en la variable mensaje almacenara los datos 
+	//escritos en los campos "mensaje" y "usuario" ademas de agregarle etiquetas html para agregar negrita y salto de linea 
+	//al momento de visualizar en el navegador web
+	$file = fopen("chat.txt", "a"); // abre el fichero en modo añadir texto("a")
+	fwrite($file, $mensaje . PHP_EOL); //escribe en el archivo // el contenido de la variable mensaje // y agrega salto de linea al texto
+	fclose($file); //cierra el archivo abierto en la variable $file
+	
+
 ?>
--- Proyecto/public/leer.php
+++ Proyecto/public/leer.php
@@ -1,6 +1,8 @@
 <?php
 $file = file("chat.txt");
-for ($i = max(0, count($file)-50); $i < count($file); $i++) {
-  echo $file[$i] . "\n";
+for ($i = max(0, count($file)-50); $i < count($file); $i++) { // en counter almacena el numero total de lineas -50
+															  // entonces si i=500 por ejemplo y menor a 550= total de lineas
+  	                                                          // si eso es verdadero	
+  echo $file[$i] . "\n";									  // mostrara del 501 hasta la linea 550 concatenando salto de linea
 }
 ?>
